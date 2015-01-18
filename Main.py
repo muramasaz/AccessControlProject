26204ad4d14d2f3dfd263f0cffd79978c1940524
muramasaz <muramasa.shin@gmail.com>
2015-01-18 10:10:38 +0700

    Fix: Include setting data from text file

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ __author__ = 'mysterylz'
 
 from readerClass import*
 from ClientThread import*
-from Functions import*
+# from Functions import*
 import fileinput
 #from serial.tools import list_ports
 
@@ -32,14 +32,15 @@ try:
         else:
            readerPacket = reader.SINGLEPACKET
 
-    ReaderName = settingList[0]
+    ReaderName = str(settingList[0]).rstrip()
     baud = int(settingList[1])
     databit = int(settingList[2])
 
     # Server IP & PORT
-    HOST = settingList[3]
+    HOST = str(settingList[3]).rstrip()
     PORT = int(settingList[4])
     TIMEOUT = int(settingList[5])
+    # print "HOST: {0} POST: {1}".format(HOST, PORT)
 
 except:
     # Set to default
@@ -54,17 +55,17 @@ except:
 # print auto_detect_serial_unix()
 
 # port = "/dev/ttyUSB1"
-
 serialport = auto_detect_serial_unix()
-
-reader.clientsocket.setting(ReaderName, HOST, PORT, TIMEOUT)
+serialport = 'COM3'
+# reader.clientsocket.setting(ReaderName, HOST, PORT, TIMEOUT)
+reader.SettingServer(HOST, PORT, TIMEOUT)
 #--------------End Init Property------------------#
 
 
 count = 0
 print "Searching serial port"
-# while serialport == "":
-#     serialport = auto_detect_serial_unix()
+while serialport == "":
+    serialport = auto_detect_serial_unix()
 
 print "Setting Reader"
 while not reader.SettingReader(ReaderName, readerType, serialport, baud, databit, readerPacket):
@@ -73,12 +74,13 @@ count = 0
 print "Setting Reader OK"
 time.sleep(0.1)
 print "Connecting to Reader"
-# while not reader.OpenPort():
-#     count += 1
-# count = 0
+while not reader.OpenPort():
+    count += 1
+count = 0
 print "Reader connected"
 time.sleep(0.1)
 print "Connecting to Server"
+# print "Host IP: {0} Port: {1}".format(reader._TCP_IP, reader._TCP_PORT)
 while not reader.StartServer():
     count += 1
 count = 0
@@ -86,6 +88,7 @@ print "Server connected"
 # print "Host IP: {0} Host Port: {1}".format(reader.clientsocket.TCP_IP, reader.clientsocket.TCP_PORT)
 time.sleep(0.1)
 print "Ready.."
+reader.SaveSettingToFile()
 
 while True:
     check = reader.StartReader(reader._CARDANDPASSWORD)
